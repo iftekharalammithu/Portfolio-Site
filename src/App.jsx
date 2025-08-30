994c37904e31a6bcfe1088dca0322b6325c7e446
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2025-08-30 23:17:47 +0600

    Update google  analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import { Navbar } from "./Components/Navbar";
 import { Header } from "./Components/Header";
 import Work from "./Pages/Work/Work";
 import Footer from "./Pages/Footer/Footer";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Send page view event whenever route changes
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
     <>
       <Transition>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Pagenotfound from "./Pages/Not-Found/Page-not-Found.jsx";
+import ReactGA from "react-ga4";
 
+// Initialize with your Measurement ID
+ReactGA.initialize("G-2WCZ4Z7XGC");
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
